693eb702a1cd534a46282b0d8c898d0e36259c9c
Anthony Ramos <ajramoslebron@gmail.com>
2026-01-07 17:47:59 -0600

    started work on thread page

---
+++ ChanBoardModernized.Shared.Components/DTOs/ThreadDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChanBoardModernized.Shared.Components.DTOs;
+
+public class ThreadDTO
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+    public List<string> Tags { get; set; } = new List<string>();
+}
