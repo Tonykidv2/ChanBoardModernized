a5645c682a47256d87c6e0fa8c7ca1d4b59caa97
Anthony Ramos <ajramoslebron@gmail.com>
2026-05-15 15:46:03 -0500

    fixing nginx settings

--- ChanBoardModernized/ChanBoardModernized/MauiProgram.cs
+++ ChanBoardModernized/ChanBoardModernized/MauiProgram.cs
@@ -63,19 +63,19 @@ namespace ChanBoardModernized
 
         static string GetApiBaseUrl()
         {
-#if ANDROID
-            // Android emulator → host machine
-            return "https://10.0.2.2:32001/";
-#elif IOS
-                // iOS simulator → host machine
-                return "https://localhost:32001/";
-#elif WINDOWS
-                return "http://localhost:8080/";
-#else
-                return "https://localhost:8080/";
-#endif
+//#if ANDROID
+//            // Android emulator → host machine
+//            return "https://10.0.2.2:32001/";
+//#elif IOS
+//                // iOS simulator → host machine
+//                return "https://localhost:32001/";
+//#elif WINDOWS
+//                return "http://localhost:8080/";
+//#else
+//                return "https://localhost:8080/";
+//#endif
 
-            //return "http://localhost:5008/";
+            return "http://localhost:5080/";
         }
     }
 }
